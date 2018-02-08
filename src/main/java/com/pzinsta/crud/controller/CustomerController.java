dc6736ef8e7e6b513a83859d0b38b078859c08cf
pzinsta <voodooshake@gmail.com>
2018-02-08 22:59:17 +0200

    Constraint requests to GET requests

--- src/main/java/com/pzinsta/crud/controller/CustomerController.java
+++ src/main/java/com/pzinsta/crud/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.pzinsta.crud.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pzinsta.crud.service.CustomerService;
@@ -14,7 +15,7 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
-    @RequestMapping("/list")
+    @GetMapping("/list")
     public String listCustomers(Model model) {
         model.addAttribute("customers", customerService.getCustomers());
         return "listCustomers";
